1becebf405a7c0eefafd9662e126289d94ecf9b9
renatoirina <irinarenato793@gmail.com>
2024-06-09 16:20:02 +0200

    fix: fixed the print of cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
     <div class="home-cont d-flex justify-content-center flex-wrap">
-        @foreach ($products as $product)
-            @include('partials.product-card')
-        @endforeach
-        
+        @include('partials.product-card')
     </div>
 @endsection
--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -1,23 +1,23 @@
 <div class="container">
     <div class="row">
         @foreach($products as $product)
-            <div class="col-12 col-md-6 col-lg-4 mb-4">
-                <div class="ms-card flex-grow-1">
-                    <!--IMAGES -->
-                    <div class="images">
-                        <img class="imgnormal img-fluid" src="{{ asset('img/'.$product['frontImage']) }}" alt="Product Image">
-                    </div>
-                    <!-- IMAGES -->
-
-                    {{-- ITEM INFO --}}
-                    <div class="item-info text-center">
-                        <a class="brand" href="">{{ $product['brand'] }}</a>
-                        <h3>{{ $product['name'] }}</h3>
-                        <span class="newprice">{{ $product['price'] }}€</span>
-                    </div>
-                    {{-- ITEM INFO --}}
+        <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <div class="ms-card flex-grow-1">
+                <!--IMAGES -->
+                <div class="images">
+                    <img class="imgnormal img-fluid" src="{{ asset('img/'.$product['frontImage']) }}" alt="Product Image">
                 </div>
+                <!-- IMAGES -->
+
+                {{-- ITEM INFO --}}
+                <div class="item-info text-center">
+                    <a class="brand" href="">{{ $product['brand'] }}</a>
+                    <h3>{{ $product['name'] }}</h3>
+                    <span class="newprice">{{ $product['price'] }}€</span>
+                </div>
+                {{-- ITEM INFO --}}
             </div>
+        </div>
         @endforeach
     </div>
 </div>
